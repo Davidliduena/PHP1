1070a48ae900f00c9da48dac4baa7c27aa6f2264
David Lidueña <davidliduena13@gmail.com>
2024-05-10 21:52:44 -0500

    cambios

---
+++ listado_productos.php
@@ -0,0 +1,57 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$aProductos = array();
+$aProductos[] = array("nombre" => "Smart TV 55\" 4K UHD",
+    "marca" => "Hitachi",
+    "modelo" => "554KS20",
+    "stock" => 60,
+    "precio" => 58000
+);
+$aProductos[] = array("nombre" => "Samsung Galaxy A30 Blanco",
+    "marca" => "Samsung",
+    "modelo" => "Galaxy A30",
+    "stock" => 0,
+    "precio" => 22000,
+);
+$aProductos[] = array("nombre" => "Aire Acondicionado Split Inverter Frío/Calor Surrey 2900F",
+    "marca" => "Surrey",
+    "modelo" => "553AIQ1201E",
+    "stock" => 5,
+    "precio" => 45000,
+);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Listado productos</title>
+</head>
+
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 text-center py-5">
+                <h1>Listado productos</h1>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <table class="table border table-hover">
+                       
+                    </table>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
--- rand.php
+++ rand.php
@@ -4,6 +4,12 @@ ini_set('disply_startup_errors', 1);
 error_reporting(E_ALL);
 
 
+$valor = rand(1, 5);
 
+if($valor == 1 || $valor == 3 || $valor == 5) {
+    echo "El nuero $valor es impar";
+}else{
+    echo "El numero $valor es par";
+}
 
 ?>
