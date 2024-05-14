b3e3c91f93aad7834e984f0f8c95ba42b8bce440
David Lidueña <davidliduena13@gmail.com>
2024-05-14 12:19:26 -0500

    advances

---
+++ numeros_pares.php
@@ -0,0 +1,42 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Numeros pares</title>
+</head>
+
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 text-center py-5">
+                <h1>Numeros Pares</h1>
+            </div>
+
+            <?php
+            for ($i = 2; $i <= 100; $i+=2) {
+                echo $i . "<br>";
+                    if($i == 60){
+                     break;
+                }
+            }
+
+            ?>
+
+
+        </div>
+    </main>
+
+</body>
+
+</html>
---
+++ sumar_total.php
@@ -0,0 +1,90 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$aProductos = array();
+$aProductos[] = array("nombre" => "Smart TV 55\" 4K UHD",
+    "marca" => "Hitachi",
+    "modelo" => "554KS20",
+    "stock" => 60,
+    "precio" => 58000
+);
+$aProductos[] = array("nombre" => "Samsung Galaxy A30 Blanco",
+    "marca" => "Samsung",
+    "modelo" => "Galaxy A30",
+    "stock" => 0,
+    "precio" => 22000,
+);
+$aProductos[] = array("nombre" => "Aire Acondicionado Split Inverter Frío/Calor Surrey 2900F",
+    "marca" => "Surrey",
+    "modelo" => "553AIQ1201E",
+    "stock" => 5,
+    "precio" => 45000,
+);
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Listado productos</title>
+</head>
+
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 text-center py-5">
+                <h1>Listado productos</h1>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <table class="table border table-hover">
+                       <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Marca</th>
+                            <th>Modelo</th>
+                            <th>Stock</th>
+                            <th>Precio</th>
+                            <th>Accion</th>
+                         </tr>
+                       </thead>
+                       <tbody>
+
+                       <?php 
+                       $sumarTotal = 0;
+                            for ($contador=0; $contador < 3; $contador++) { 
+                                # code... no va nada solo de ejemplo
+                            
+                            ?>
+                        <tr>
+                        <td><?php echo $aProductos[$contador]["nombre"];?></td>
+                        <td><?php echo $aProductos[$contador]["marca"];?></td>
+                        <td><?php echo $aProductos[$contador]["modelo"];?></td>
+                        <td><?php echo $aProductos[$contador]["stock"] == 0? "no hay stock" : ($aProductos[$contador]["stock"] > 10? "Hay stock" : "Poco stock"); ?></td>
+                        <td><?php echo $aProductos[$contador]["precio"];?></td>
+                        <td><button class="btn btn-primary">comprar</button></td>
+                        </tr>
+                            <?php 
+                                $contador;
+                            }?>
+                       </tbody>
+                    </table>
+                    <div class="row">
+                        <div class="col-12">
+                            <p>La sumatoria de los precios es: <?php echo $sumarTotal ?></p>
+                        </div>
+                       </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
